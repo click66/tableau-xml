ee27405af8200ab78693034f45177641262832cc
kane holt <kane.holt@efficioconsulting.com>
2022-12-07 14:58:15 +0000

    Updated NONE to be None

--- Tableau_Formatter.py
+++ Tableau_Formatter.py
@@ -29,11 +29,14 @@ def update_font(wb, new_font, logging=None):
     else:
         if matched_flag == False:
             log(logging, "Font could not be changed")
+    
+
 
     return wb
     # # Get the XML we read in above and write the new element
     # tree = ET.ElementTree(root)
-    # tree.write(wbname, xml_declaration=True, encoding='utf-8')
+     #tree.write(wb.xml, xml_declaration=True, encoding='utf-8')
+
 
 
 # Outer Padding
@@ -49,7 +52,7 @@ def update_outerpadding(wb, new_outer_padding, logging=None):
     # child = tree.findall('dashboards/dashboard/zones/zone/zone-style/format/[@attr="margin"]')
 
     for child in tree.findall(".//*[@type-v2='filter']/zone-style/format/[@attr='margin']"):
-        if child is not NONE:
+        if child is not None:
             child.set("value", new_outer_padding)
             matched_flag = True
         log(logging, "Outer padding has been changed")
@@ -59,7 +62,7 @@ def update_outerpadding(wb, new_outer_padding, logging=None):
             log(logging,"Outer padding could not be changed")
 
     for child in tree.findall(".//*[@type-v2='filter']/zone-style/format/[@attr='margin-top']"):
-        if child is not NONE:
+        if child is not None:
             child.set("value", new_outer_padding)
             matched_flag = True
         log(logging,"Outer padding has been changed")
